7c6127447bad56fe26b22d13987065e791c889cd
Sipke Schoorstra <sipke@ideliverable.com>
2015-05-17 20:28:37 +0200

    Removed AjaxWidget.
    Use AjaxifyPart instead to ajaxify widgets or the Content Widget (OOB since Orchard 1.9) to render content in-place.

--- Migrations/AjaxWidgetMigrations.cs
+++ Migrations/AjaxWidgetMigrations.cs
@@ -7,19 +7,10 @@ namespace IDeliverable.Widgets.Migrations {
     [OrchardFeature("IDeliverable.Widgets.Ajax")]
     public class AjaxWidgetMigrations : DataMigrationImpl {
         public int Create() {
-            ContentDefinitionManager.AlterPartDefinition("AjaxWidgetPart", part => part
-                .Attachable(false)
-                .WithDescription("A widget used to load content and other widgets asynchronously via AJAX."));
-
+            
             ContentDefinitionManager.AlterPartDefinition("AjaxifyPart", part => part
                 .Attachable()
                 .WithDescription("Turns an existing widget type into one that can be loaded asynchronously via AJAX."));
-
-            ContentDefinitionManager.AlterTypeDefinition("AjaxWidget", type => type
-                .WithPart("CommonPart")
-                .WithPart("WidgetPart")
-                .WithPart("AjaxWidgetPart")
-                .WithSetting("Stereotype", "Widget"));
 
             return 1;
         }
